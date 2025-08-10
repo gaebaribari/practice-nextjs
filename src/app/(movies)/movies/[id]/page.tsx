5d13630c3f2e0a21ab9845cdac3ed56aa771638d
gaebaribari <gaebaribari@gmail.com>
2025-08-10 13:21:52 +0900

    practice: loading, suspense, error handling

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,26 @@
-export default function MovieDetail({
+import MovieInfo from "@/components/movie-info";
+import MovieVideos from "@/components/movie-videos";
+import { Suspense } from "react";
+
+export default async function MovieDetail({
 	params: { id },
 }: {
 	params: { id: string };
 }) {
-	return <h1>Movie {id}</h1>;
+	return (
+		<div>
+			<h3>Movie detail page</h3>
+			<div className="my-2">
+				<Suspense fallback={<h1>Loading movie info</h1>}>
+					<MovieInfo id={id} />
+				</Suspense>
+			</div>
+
+			<div>
+				<Suspense fallback={<h1>Loading movie videos</h1>}>
+					<MovieVideos id={id} />
+				</Suspense>
+			</div>
+		</div>
+	);
 }
